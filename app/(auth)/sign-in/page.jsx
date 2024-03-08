68382c213c3738ebb328a3ac7399ef6e9bc6a3bf
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-03-09 00:16:26 +0530

    auth + routing added using clerk

--- app/(auth)/sign-in/page.jsx
+++
@@ -1,9 +0,0 @@
-const SignIn = () => {
-  return (
-    <main className="flex h-screen w-full items-center justify-center bg-blue-200">
-      <h1 className="text-2xl">Sign In</h1>
-    </main>
-  );
-};
-
-export default SignIn;
--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 const page = () => {
   return (
-    <main className="flex h-screen w-full items-center justify-center bg-pink-200">
+    <div className="flex h-screen w-full items-center justify-center bg-gray-300">
+      <UserButton />
       <h1 className="text-2xl">Home Page, Start the Game</h1>
-    </main>
+    </div>
   );
 };
 
